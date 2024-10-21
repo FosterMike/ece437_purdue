85fea5bd331cbcd4a48759d62cb06164f5a34b65
FosterMike <michaelfoster.purdue@gmail.com>
2024-10-21 19:41:23 -0400

    Update mergsort.asm
    Removed all but one nop

--- mergsort.asm
+++ mergsort.asm
@@ -8,21 +8,21 @@
 org 0x0000
  ori   $8, $0, 0xFFC
   ori   $2, $0, 0xFFC
-  nop
+  
   lui $3, 0xffff7  
-  nop
+  
  sub $2, $2, $3
  sub $8, $8, $3
- nop
+ 
  lui $3, 0x00007
- nop
+ 
  add $2, $2, $3
  add $8, $8, $3
- nop
+ 
   ori   $12, $0, data
-  nop
+  
   lw    $24, size($0)
-  nop
+  
   ori   $6, $0, 1
   srl  $13,$24,$6
   or    $9, $0, $12
@@ -40,17 +40,17 @@ org 0x0000
   jal   insertion_sort
  or    $12, $0, $9
   or    $13, $0, $18
-  nop
+  
   or    $14, $0, $19
-  nop
+  
   or    $15, $0, $20
-  nop
+  
   ori   $5, $0, sorted
-  nop
+  
   
   // push  $5
     addi $2, $2, -4
-    nop
+    
     sw $5, 0($2)
   
   jal   merge
@@ -64,34 +64,34 @@ org 0x0000
 # $a1 : size of array
 #--------------------------------------
 insertion_sort:
-nop
+
  ori   $5, $0, 4
   ori   $7, $0, 2
-  nop
+  
   sll  $6,$13,$7
 is_outer:
  sltu  $4, $5, $6
   beq   $4, $0, is_end
   add  $31, $12, $5
-    nop
   lw    $30, 0($31)
-    nop
+  nop               // Needs NOP here to function
+    
 is_inner:
  beq   $31, $12, is_inner_end
-    nop
+    
   lw    $16, -4($31)
-    nop
+    
   slt   $4, $30, $16
   beq   $4, $0, is_inner_end
-    nop
+    
   sw    $16, 0($31)
-    nop
+    
   addi $31, $31, -4
   j     is_inner
 is_inner_end:
- nop
+ 
  sw    $30, 0($31)
- nop
+ 
   addi $5, $5, 4
   j     is_outer
 is_end:
@@ -106,9 +106,9 @@ is_end:
 # dst [$sp+4] : pointer to merged list location
 #--------------------------------------
 merge:
- nop
+ 
  lw    $5, 0($2)
- nop
+ 
 m_1:
  bne   $13, $0, m_3
 m_2:
@@ -117,50 +117,50 @@ m_2:
 m_3:
  beq   $15, $0, m_4
   beq   $13, $0, m_5
-  nop
+  
   lw    $6, 0($12)
-  nop
+  
   lw    $7, 0($14)
-  nop
+  
   slt   $4, $6, $7
   beq   $4, $0, m_3a
-  nop
+  
   sw    $6, 0($5)
-  nop
+  
   addi $5, $5, 4
   addi $12, $12, 4
   addi $13, $13, -1
-  nop
+  
   j     m_1
 m_3a:
- nop
+ 
  sw    $7, 0($5)
- nop
+ 
   addi $5, $5, 4
   addi $14, $14, 4
   addi $15, $15, -1
   j     m_1
 m_4:  #left copy
- nop
+ 
  lw    $6, 0($12)
-  nop
+  
   sw    $6, 0($5)
-  nop
+  
   addi $5, $5, 4
   addi $13, $13, -1
   addi $12, $12, 4
-  nop
+  
   beq   $13, $0, m_end
   j     m_4
 m_5:  # right copy
  lw    $7, 0($14)
-  nop
+  
   sw    $7, 0($5)
-  nop
+  
   addi $5, $5, 4
   addi $15, $15, -1
   addi $14, $14, 4
-  nop
+  
   beq   $15, $0, m_end
   j     m_5
 m_end:
